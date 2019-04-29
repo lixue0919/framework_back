dbea06368e91db883b53f2a49a763331df70c081
lixue0919 <442019696@qq.com>
2019-04-29 11:37:24 +0800

    1

--- userservice/src/main/java/com/syfri/userservice/UserserviceApplication.java
+++ userservice/src/main/java/com/syfri/userservice/UserserviceApplication.java
@@ -1,11 +1,9 @@
 package com.syfri.userservice;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.ServletComponentScan;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
